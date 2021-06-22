224047c84f5f68c98a608f7dd46e1ab08833195c
shylton <mattas@oregonstate.edu>
2021-06-22 16:20:10 -0400

    navbar and router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Container from '@material-ui/core/Container'
 
-import Navbar from './components/layout/Navbar'
-
+import Navbar from './components/Navbar'
+import Homepage from './components/Homepage'
+import About from './components/About'
 
 export default function App() {
 
   return (
-    <Navbar />
+    <Router>
+      <React.Fragment>
+        <Navbar />
+        <Container>
+          <Switch>
+            <Route exact path='/' component={Homepage} />
+            <Route path='/about' component={About} />
+          </Switch>
+        </Container>
+      </React.Fragment>
+    </Router>
   )
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+
 import { makeStyles } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
     title: {
         flexGrow: 1
+    },
+    navbarLink: { // refactor: this should be a global change?
+        color:'white',
+        textDecoration:'none'
     }
 }))
 
@@ -18,8 +25,18 @@ export default function Navbar({ title, icon }) {
         <AppBar position='static'>
             <Toolbar>
                 <Typography variant='h6' className={classes.title}>
-                    <i className={icon}></i> {title}
+                    <i className={icon} /> {title}
                 </Typography>
+                <Button>
+                    <Link to="/" className={classes.navbarLink}>
+                        Home
+                    </Link>
+                </Button>
+                <Button>
+                    <Link to="/about" className={classes.navbarLink}>
+                        About
+                    </Link>
+                </Button>
             </Toolbar>
         </AppBar>
     )
